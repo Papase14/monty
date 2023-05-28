67cbd6a16f7e31808cdfa0c57f3d195b9af93eb2
Tshupane Morake <tshupane@outlook.com>
2023-05-28 20:35:52 +0200

    Main

--- main.c
+++ main.c
@@ -27,41 +27,27 @@ void opcode(char *command, unsigned int line_num, stack_t **stack)
 {
 	int i = 0;
 	instruction_t ops[] = {
-		{"push", push},
-		{"pall", pall},
-		{"pint", pint},
-		{"pop", pop},
-		{"swap", swap},
-		{"add", add},
-		{"nop", nop},
-		{"sub", sub},
-		{"div", _div},
-		{"mul", mul},
-		{"mod", mod},
-		{"pchar", pchar},
-		{"pstr", pstr},
-		{"rotl", rotl},
-		{"rotr", rotr},
-		{NULL, NULL}
+		{"push", push}, {"pall", pall}, {"pint", pint}, {"pop", pop}, {"swap", swap},
+		{"add", add}, {"nop", nop}, {"sub", sub}, {"div", _div}, {"mul", mul},
+		{"mod", mod}, {"pchar", pchar}, {"pstr", pstr}, {"rotl", rotl},
+		{"rotr", rotr}, {NULL, NULL}
 	};
 
-	if (command[0] == '#')
-	{
+	if(command[0] == '#')
 		return;
-	}
-	if (strcmp(command, "stack") == 0)
+	if(strcmp(command, "stack") == 0)
 	{
 		arg_holder.SQ = 1;
 		return;
 	}
-	if (strcmp(command, "queue") == 0)
+	if(strcmp(command, "queue") == 0)
 	{
 		arg_holder.SQ = 0;
 		return;
 	}
-	while (ops[i].opcode != NULL)
+	while(ops[i].opcode != NULL)
 	{
-		if (strcmp(ops[i].opcode, command) == 0)
+		if(strcmp(ops[i].opcode, command) == 0)
 		{
 			ops[i].f(stack, line_num);
 			return;
@@ -79,15 +65,13 @@ void opcode(char *command, unsigned int line_num, stack_t **stack)
  */
 void free_stack(stack_t **stack)
 {
-    stack_t *current = *stack;
-    stack_t *next;
+	stack_t *current = *stack;
+	stack_t *next;
 
-    while (current != NULL)
-    {
-        next = current->next;
-        free(current);
-        current = next;
-    }
-
-    *stack = NULL;
+	while(current != NULL)
+	{
+		next = current->next;
+		free(current);
+		current = next;
+	} *stack = NULL;
 }
